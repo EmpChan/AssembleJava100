9060cbbeaa1461ed87bf0d5746b33dee5b9c6b87
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2023-10-18 10:08:15 +0900

    Create Java67.java

---
+++ 해결한문제/Java67.java
@@ -0,0 +1,13 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        while(a > 0){
+            System.out.println(a--);
+        }
+    }
+}
