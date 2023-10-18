eee7f076a344dd67220ff98f18ea4c76eb0de9fd
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2023-10-18 10:00:21 +0900

    Create Java63.java

---
+++ 해결한문제/Java63.java
@@ -0,0 +1,11 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a*24);
+    }
+}
