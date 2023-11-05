85ec1acbac3bd951427016d755dd7f69f9017d25
boxty123 <129060531+boxty123@users.noreply.github.com>
2023-11-05 23:36:13 +0900

    Add files via upload

---
+++ src/java_ex100/Java22.java
@@ -0,0 +1,16 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class Java22 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a>b)System.out.println("1");
+		else System.out.println("0");
+		
+	}
+
+}
