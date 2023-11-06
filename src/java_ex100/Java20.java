8a9cdd34eaacb635260e6fd6a33d9b6b65952ba2
boxty123 <129060531+boxty123@users.noreply.github.com>
2023-11-06 15:49:15 +0900

    Add files via upload

---
+++ src/java_ex100/Java20.java
@@ -0,0 +1,14 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class Java20 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();int b=sc.nextInt();int c=sc.nextInt();
+		long sum=a+b+c;
+		System.out.println(sum);
+		System.out.println(sum/3.0);
+	}
+}
