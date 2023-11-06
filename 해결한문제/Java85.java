82d65f976237c3608b82f67fc8cb2ff236cccb74
Kim-SuYeong <74577728+Kim-SuYeong@users.noreply.github.com>
2023-11-06 15:08:07 +0900

    Create Java85.java

---
+++ 해결한문제/Java85.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n =scan.nextInt();
+        for (int i=n; i>0; i--) {
+            for (int j=i; j>0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
