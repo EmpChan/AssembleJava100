a9dbc355f929d63f5a5261db0d482017960b1c63
Taehyung Kim <122429423+openswthkim@users.noreply.github.com>
2023-11-07 15:24:53 +0900

    Create Java10.java

---
+++ Java10.java
@@ -0,0 +1,17 @@
+package practice;
+import java.util.Scanner;
+
+public class Java10 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.print(a+" "+b);
+		
+		scanner.close();
+
+	}
+
+}
