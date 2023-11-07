08468b2634a36e0484f95b07a8c93de9bc1e75e9
Taehyung Kim <122429423+openswthkim@users.noreply.github.com>
2023-11-07 15:27:21 +0900

    Create Java16.java

---
+++ 해결한문제/Java16.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Java16 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		
+		System.out.printf("%o",a);
+
+		scanner.close();
+	}
+
+}
