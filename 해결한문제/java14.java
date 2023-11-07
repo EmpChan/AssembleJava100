6b6fbdaa1a384c68d045515229b0523075772452
wylee1 <122427710+wylee1@users.noreply.github.com>
2023-11-07 15:16:39 +0900

    Create java14.java

---
+++ 해결한문제/java14.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Scanner;
+
+public class java14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        System.out.println(hour + ":" + minute);
+        
+        sc.close();
+    }
+}
