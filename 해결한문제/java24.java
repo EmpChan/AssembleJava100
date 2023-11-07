ee2da4ef025c7d4581c8fa9e8169c13e5aaeb005
wylee1 <122427710+wylee1@users.noreply.github.com>
2023-11-07 15:16:53 +0900

    Create java24.java

---
+++ 해결한문제/java24.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class java24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(b>=a) {
+        	System.out.println("1");
+        }else {
+        	System.out.println("0");
+        }
+        sc.close();
+    }
+}
