23e0a5d6d3b8ef335e725587333efb3b3ef578ef
wylee1 <122427710+wylee1@users.noreply.github.com>
2023-11-14 00:39:09 +0900

    Create Java44.java

---
+++ 해결한문제/Java44.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Java44 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==1) {
+        	System.out.println("oh my god");
+        }else {
+        	System.out.println("enjoy");
+        }
+        sc.close();
+    }
+}
