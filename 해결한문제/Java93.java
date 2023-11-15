05a80ddb95ff5ed53b0efca49eef82858115b0ea
Kim-SuYeong <74577728+Kim-SuYeong@users.noreply.github.com>
2023-11-15 15:47:18 +0900

    Update Java93.java

--- 해결한문제/Java93.java
+++ 해결한문제/Java93.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Java93 {
+public class Main {
     public static void main(String[] args) {
         String[] words = new String[7];
         Scanner scan = new Scanner(System.in);
