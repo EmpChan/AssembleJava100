04fa0e8a80d698675b69b674e6d305eaf61dc87b
Kim-SuYeong <74577728+Kim-SuYeong@users.noreply.github.com>
2023-11-15 15:45:17 +0900

    Create Java93.java

---
+++ 해결한문제/Java93.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Java93 {
+    public static void main(String[] args) {
+        String[] words = new String[7];
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 7; i++)
+            words[i] = scan.nextLine();
+
+        for (int i = 0; i < 7; i++)
+            System.out.println(i + "번째 단어: " + words[i]);
+        try {
+            Thread.sleep(5000);
+            for (int j = 0; j < 20; j++)
+                System.out.println();
+        } catch (Exception e) {
+            return;
+        }
+
+        int k = scan.nextInt();
+        System.out.println(k + "번째 단어: " + words[k]);
+    }
+}
